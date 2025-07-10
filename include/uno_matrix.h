66e4cd3c946458d326196e44cbca008776e53604
olegt <oleg@toropov.net>
2025-07-09 22:20:49 -0400

    Tested output icons

--- include/uno_matrix.h
+++ include/uno_matrix.h
@@ -3,8 +3,6 @@
 #include <Arduino.h>
 #include <Arduino_LED_Matrix.h>
 
-ArduinoLEDMatrix matrix;
-
 enum IconId {
   ICON_SMILEY = 0,
   ICON_EXCLAM_LEFT = 1,
@@ -12,10 +10,6 @@ enum IconId {
   ICON_EXCLAM_BOTH = 3
 };
 
-inline const uint32_t EXCLAMATION_LEFT[] = {0x30030030, 0x3003000, 0x300300};
-inline const uint32_t EXCLAMATION_RIGHT[] = {0xc00c00, 0xc00c00c0, 0xc00c};
-inline const uint32_t EXCLAMATION_BOTH[] = {0x30c30c30, 0xc30c30c0, 0x30c30c};
-
 void ShowIconById(IconId iconId);
-
+void printIcon12x8(const uint32_t icon[]);
  #endif
--- src/uno_matrix.cpp
+++ src/uno_matrix.cpp
@@ -1,9 +1,16 @@
 #include "uno_matrix.h"
 
+extern ArduinoLEDMatrix matrix; 
+
+
+
+const uint32_t* icon = nullptr;
+const uint32_t EXCLAMATION_LEFT[] = {0x30030030, 0x3003000, 0x300300};
+const uint32_t EXCLAMATION_RIGHT[] = {0xc00c00, 0xc00c00c0, 0xc00c};
+const uint32_t EXCLAMATION_BOTH[] = {0x30c30c30, 0xc30c30c0, 0x30c30c};
 
 void ShowIconById(IconId iconId) {
-  const uint32_t* icon = nullptr;
-
+ 
   switch (iconId) {
     case ICON_SMILEY:
       icon = LEDMATRIX_EMOJI_HAPPY;
@@ -21,6 +28,19 @@ void ShowIconById(IconId iconId) {
       matrix.clear();  // Safe fallback for invalid ID
       return;
   }
-
+  // we are here only if the icon was assigned
   matrix.loadFrame(icon);
+}
+
+void printIcon12x8(const uint32_t icon[]) {
+  for (int row = 0; row < 8; row++) {
+    for (int col = 0; col < 12; col++) {
+      int bitIndex = row * 12 + col;
+      int wordIndex = bitIndex / 32;
+      int bitInWord = 31 - (bitIndex % 32);
+      bool bit = (icon[wordIndex] >> bitInWord) & 0x1;
+      Serial.print(bit ? '#' : '.');
+    }
+    Serial.println();
+  }
 }
